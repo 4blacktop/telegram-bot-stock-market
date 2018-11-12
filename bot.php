82b1fac18a112e8a705296d319a60e29437c53cf
4blacktop <4blacktop@gmail.com>
2018-11-12 20:48:12 +1000

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 // http://www.fileformat.info/info/emoji/list.htm
-define('BOT_TOKEN', '236412659:AAGa5NG2JhbaypK64CCx1lXFPQ_PUJM1dww');
+define('BOT_TOKEN', '23641sdghsfgjsPQ_PUJM1dww');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
